e8ae3c865e9d0c3d28de8a82b5dd120e2000576d
Sricharan Gali <charangali3@gmail.com>
2016-08-17 09:01:23 +0530

    few changes

--- app/src/main/java/main/charansapp/com/autotext/MainActivity.java
+++ app/src/main/java/main/charansapp/com/autotext/MainActivity.java
@@ -1,5 +1,6 @@
 package main.charansapp.com.autotext;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,11 +9,30 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener
+{
+    AutoCompleteTextView auto;
+    TextView textView;
+    ToggleButton tg1,tg2;
+    Button b1,b2;
+    public ProgressDialog progress;
+    String[] arr = { "Paries,France", "PA,United States","Parana,Brazil", "Padua,Italy", "Pasadena,CA,United States"};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -26,27 +46,90 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        b2 = (Button) findViewById(R.id.button);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+        List<String> categories = new ArrayList<String>();
+        categories.add("Java");
+        categories.add("Android");
+        categories.add("Hadoop");
+        categories.add("Bigdata");
+        categories.add("Javascript");
+        categories.add("pearl");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+
+
+        auto = (AutoCompleteTextView)
+                findViewById(R.id.auto);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (this,android.R.layout.select_dialog_item, arr);
+
+        auto.setThreshold(2);
+        auto.setAdapter(adapter);
+        CharSequence sequence = auto.getText();
+        textView = (TextView) findViewById(R.id.dest);
+        textView.setText(sequence);
+        Toast.makeText(this,sequence,Toast.LENGTH_SHORT).show();
+
+        tg1=(ToggleButton)findViewById(R.id.toggleButton2);
+        tg2=(ToggleButton)findViewById(R.id.toggleButton);
+
+        b1=(Button)findViewById(R.id.button2);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringBuffer result = new StringBuffer();
+                result.append(" You have clicked first Toggle Button as ").append(tg1.getText());
+                result.append("\n You have clicked Second Toggle Button as ").append(tg2.getText());
+                Toast.makeText(MainActivity.this, result.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void download(View view){
+        progress=new ProgressDialog(this);
+        progress.setMessage("Downloading CandyCrush");
+        progress.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progress.setIndeterminate(true);
+        progress.setProgress(0);
+        progress.show();
+
+        final int totalProgressTime = 100;
+        final Thread t = new Thread() {
+            @Override
+            public void run() {
+                int jumpTime = 0;
+
+                while(jumpTime < totalProgressTime) {
+                    try {
+                        sleep(200);
+                        jumpTime += 5;
+                        progress.setProgress(jumpTime);
+                    }
+                    catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        t.start();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+    {
+        String item = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onNothingSelected(AdapterView<?> parent)
+    {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
